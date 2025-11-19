f4645def92c2c5b27cc1eb96ff95ede5a451f529
Sanghoon Lee <gcccompil3r@gmail.com>
2025-11-19 16:54:55 +0900

    feat: ready to analysis documents

--- documents/infrastructure/repository/document_repository_impl.py
+++ documents/infrastructure/repository/document_repository_impl.py
@@ -1,6 +1,11 @@
 from typing import List
+from sqlalchemy.orm import Session
+
+from config.database.session import SessionLocal
 from documents.application.port.document_repository_port import DocumentRepositoryPort
 from documents.domain.document import Document
+from documents.infrastructure.orm.document_orm import DocumentORM
+
 
 class DocumentRepositoryImpl(DocumentRepositoryPort):
     __instance = None
@@ -8,27 +13,55 @@ class DocumentRepositoryImpl(DocumentRepositoryPort):
     def __new__(cls, *args, **kwargs):
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)
-            cls.__instance.repository = DocumentRepositoryImpl.getInstance()
-
         return cls.__instance
 
     @classmethod
     def getInstance(cls):
         if cls.__instance is None:
             cls.__instance = cls()
-
-            cls.__instance._documents = []
-            cls.__instance._id_counter = 1
-
         return cls.__instance
 
     def save(self, document: Document) -> Document:
-        if document.id is None:
-            document.id = self._id_counter
-            self._id_counter += 1
 
-        self._documents.append(document)
+        db: Session = SessionLocal()
+        try:
+            db_obj = DocumentORM(
+                file_name=document.file_name,
+                s3_key=document.s3_key,
+                uploader_id=document.uploader_id,
+            )
+            db.add(db_obj)
+            db.commit()
+            db.refresh(db_obj)
+
+            # DB에서 받은 id와 timestamp를 도메인 객체에 반영
+            document.id = db_obj.id
+            document.uploaded_at = db_obj.uploaded_at
+            document.updated_at = db_obj.updated_at
+        finally:
+            db.close()
+
         return document
 
     def find_all(self) -> List[Document]:
-        return self._documents
+        """
+        DB에 있는 모든 문서를 도메인 객체로 반환
+        """
+        db: Session = SessionLocal()
+        documents: List[Document] = []
+        try:
+            db_objs = db.query(DocumentORM).all()
+            for obj in db_objs:
+                doc = Document(
+                    file_name=obj.file_name,
+                    s3_key=obj.s3_key,
+                    uploader_id=obj.uploader_id
+                )
+                doc.id = obj.id
+                doc.uploaded_at = obj.uploaded_at
+                doc.updated_at = obj.updated_at
+                documents.append(doc)
+        finally:
+            db.close()
+
+        return documents
