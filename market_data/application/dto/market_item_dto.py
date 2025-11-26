d0659ea0144d142b6ee3ddefd0d14dadb2fc0593
Sanghoon Lee <gcccompil3r@gmail.com>
2025-11-26 19:11:40 +0900

    refactor: market data 구조 변경

--- market_data/application/dto/market_item_dto.py
+++
@@ -1,6 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass
-class MarketItemDTO:
-    name: str
-    price: int
--- market_data/domain/market_data.py
+++ market_data/domain/market_data.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 from typing import List
-
-from market_data.domain.market_item import MarketItem
-
+from .market_item import MarketItem
+from .value_object.market_source import MarketSource
+from .value_object.timestamp import Timestamp
 
 class MarketData:
-    def __init__(self, items: List[MarketItem], source: str, fetched_at: datetime):
+    def __init__(self, items: List[MarketItem], source: MarketSource, fetched_at: Timestamp):
         self.items = items
         self.source = source
         self.fetched_at = fetched_at
